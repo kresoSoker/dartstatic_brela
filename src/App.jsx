8cd63984be0cf9d26ece9201f50539566069889b
kresimir <kreso2610@gmail.com>
2025-10-22 15:09:28 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <h1>Hello World 👋</h1>
+      <p>This is my first React app deployed on GitHub Pages!</p>
+    </div>
+  );
+}
+
+export default App;
